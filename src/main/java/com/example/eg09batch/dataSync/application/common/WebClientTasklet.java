1d74c8c23caa00f2d7eca4baeb20ee17884bf02e
T.Kotegawa <tkotegawa@stnet.co.jp>
2022-02-01 08:10:56 +0900

    WebClientTasklet mod

--- src/main/java/com/example/eg09batch/dataSync/application/common/WebClientTasklet.java
+++ src/main/java/com/example/eg09batch/dataSync/application/common/WebClientTasklet.java
@@ -17,14 +17,31 @@ import reactor.core.publisher.Mono;
 @StepScope
 public class WebClientTasklet implements Tasklet {
 
-    WebClient client = WebClient.create("https://api.github.com/users/defunkt");
+    private final String BASE_URL = "https://api.github.com/users/defunkt";
+
+    private final String ADDRESS_BOOK_ID = "abc";
+
+    WebClient client = WebClient.builder()
+            .baseUrl(BASE_URL)
+            .defaultHeader("defaultKey1", "defaultValue1") // 認証情報等を設定
+            .defaultHeader("defaultKey2", "defaultValue2")
+            .build();
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
 
-        GitResponse body = client.get().retrieve().bodyToMono(GitResponse.class).block();
-        log.info(body.toString());
+        String address_content = "CSVの中身";
 
+        String body = client.post()
+                .uri("/abook/{address_book_id}/import", ADDRESS_BOOK_ID)
+                .header("key", "value") //追加パラメータを設定
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(address_content)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        log.info(body);
 
         return RepeatStatus.FINISHED;
     }
